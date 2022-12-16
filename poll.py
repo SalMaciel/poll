db9273994a701bf4364baaa8e4d8314adf2279d8
Salvador Maciel <salmaciel@github.com>
2022-12-16 11:36:56 -0700

    fixed typos on poll file

--- poll.py
+++ poll.py
@@ -1,16 +1,20 @@
 import csv
 import os
 
-load_file = os.path.join("data", "elections_results.csv")
-save_file = os.path.join("data", "elections_results.txt")
+load_file = os.path.join("data", "election_results.csv")
+save_file = os.path.join("data", "election_results.txt")
+
+total_votes = 0
 
 # Open election results and read read file
 with open(load_file) as election_data:
-    file_reader = csv.read(election_data)
+    file_reader = csv.reader(election_data)
 
     # Read header row
     headers = next(file_reader)
 
     # Print each row in csv file
     for roe in file_reader:
-        print(row)
+        total_votes += 1
+
+print(total_votes)
