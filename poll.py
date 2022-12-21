118c8ad19c1f6eacd512f3f21ba12973d3dcf2f1
Salvador Maciel <salmaciel@github.com>
2022-12-21 16:10:41 -0700

    modified poll.py

--- poll.py
+++ poll.py
@@ -8,6 +8,13 @@ total_votes = 0
 
 candidate_options = []
 
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open election results and read read file
 with open(load_file) as election_data:
     file_reader = csv.reader(election_data)
@@ -28,4 +35,36 @@ with open(load_file) as election_data:
             # Add the candidate name to the candidate list.
             candidate_options.append(candidate_name)
 
-print(candidate_options)
+            # Initializong the vote count to 0 to all candidates.
+            candidate_votes[candidate_name] = 0
+
+        # Counting votes
+        candidate_votes[candidate_name] += 1
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # Print out each candidate's name, vote count, and percentage of
+    # votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+    # Determine winning vote count and candidate
+    # Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percent =
+        # vote_percentage.
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
