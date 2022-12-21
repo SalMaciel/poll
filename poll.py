82b430dda32b31fa5c5e310eaa81e70146143023
Salvador Maciel <salmaciel@github.com>
2022-12-21 15:33:48 -0700

    modified poll.py

--- poll.py
+++ poll.py
@@ -6,6 +6,8 @@ save_file = os.path.join("data", "election_results.txt")
 
 total_votes = 0
 
+candidate_options = []
+
 # Open election results and read read file
 with open(load_file) as election_data:
     file_reader = csv.reader(election_data)
@@ -14,7 +16,16 @@ with open(load_file) as election_data:
     headers = next(file_reader)
 
     # Print each row in csv file
-    for roe in file_reader:
-        total_votes += 1
+    for row in file_reader:
 
-print(total_votes)
+        total_votes += 1
+        
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+
+            # Add the candidate name to the candidate list.
+            candidate_options.append(candidate_name)
+
+print(candidate_options)
